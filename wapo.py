ebfe75f87c39bb96b07324f136699c2f90ddc193
timschott <tcs9pk@virginia.edu>
2020-04-03 13:49:53 -0400

    posted a tweet

--- wapo.py
+++ wapo.py
@@ -13,9 +13,38 @@ from newsapi import NewsApiClient
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+# twitter clent
+import tweepy
+
 def init_client(key):
-	if (key is not None):
-		return NewsApiClient(news_api_key = key)
+	if (key is None):
+		print ('blank news api key')
+		return null
+	return NewsApiClient(api_key = key)
+
+def init_twitter_client(api_key, secret_key, access_token, access_secret):
+
+	if (api_key is None):
+		print('twitter api key is null')
+		return null
+
+	if (secret_key is None):
+		print ('twitter secret is null')
+		return null
+
+	if (access_token is None):
+		print ('twitter token is null')
+		return null
+
+	if (access_secret is None):
+		print ('twitter secret is null')
+		return null
+
+	auth = tweepy.OAuthHandler(api_key, secret_key)
+	auth.set_access_token(access_token, access_secret)
+	client = tweepy.API(auth)
+	
+	return client
 
 """
 	information about the api call
@@ -194,22 +223,26 @@ def find_note(paragraphs):
 if __name__ == "__main__":
 
 	# initialize client 
-	news_client = init_client(s_config.news_api_key)
+	news_client = init_client(s_config.api_key)
 
 	# api call
 	article_json = api_call(news_client, "+Bezos", 'the-washington-post', '2020-03-30T00:24:52', '2020-03-31T00:24:52', 'publishedAt')
 	# print (article_json)
 
-	article_dict = get_article_dict(article_json)
+	# article_dict = get_article_dict(article_json)
 
 	# for every url
-	results = []
-	for url in article_dict.values():
+	# results = []
+	# for url in article_dict.values():
 		# paragraphs of that article
-		paras = get_article_text(url)
-		results.append(find_note(paras))
+	#	paras = get_article_text(url)
+	#	results.append(find_note(paras))
 
 	# for every result, check and tweet (or not)
 
+	twitter_client = init_twitter_client(s_config.twitter_api_key, s_config.twitter_secret_key, s_config.twitter_access_token, s_config.twitter_access_secret)
+	tweet = twitter_client.update_status("Flight test")
+
+	# api.destroy_status(tweet.id_str)
 
 
