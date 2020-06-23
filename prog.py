3ea0af0fc3d7effa44c31572629f9056d5e38a74
timschott <tcs9pk@virginia.edu>
2020-06-23 17:01:25 -0400

    also just going to put this in here

---
+++ prog.py
@@ -0,0 +1,58 @@
+## imports.
+import s_config
+import os
+
+## twitter clent
+import tweepy
+
+def env_vars():
+	keys = []
+
+	keys.append(s_config.twitter_api_key)
+	keys.append(s_config.twitter_secret_key)
+	keys.append(s_config.twitter_bearer_token)
+	keys.append(s_config.twitter_access_token)
+	keys.append(s_config.twitter_access_secret)
+	return keys
+
+def init_twitter_client(api_key, secret_key, access_token, access_secret):
+
+	if (api_key is None):
+		print('twitter api key is null')
+		return None
+
+	if (secret_key is None):
+		print ('twitter secret is null')
+		return None
+
+	if (access_token is None):
+		print ('twitter token is null')
+		return None
+
+	if (access_secret is None):
+		print ('twitter secret is null')
+		return None
+
+	auth = tweepy.OAuthHandler(api_key, secret_key)
+	auth.set_access_token(access_token, access_secret)
+	client = tweepy.API(auth)
+
+	return client
+
+if __name__ == "__main__":
+
+	key_array = env_vars()
+
+	## twitter creds
+	twitter_api_key = key_array[0]
+	twitter_secret_key = key_array[1]
+	twitter_bearer_token = key_array[2]
+	twitter_access_token = key_array[3]
+	twitter_access_secret = key_array[4]
+
+	twitter_client = init_twitter_client(twitter_api_key, twitter_secret_key, twitter_access_token, twitter_access_secret)
+
+	## use `user_timeline` and wild out. 
+
+
+
